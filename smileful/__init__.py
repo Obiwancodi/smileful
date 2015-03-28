2cb505f5292aeaebe07e7e82803f0421b2a6da8d
George Fleming <gflemingiii@gmail.com>
2015-03-28 01:46:41 +0000

    heroku stuff

--- smileful/__init__.py
+++ smileful/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 config_path = os.environ.get("CONFIG_PATH", "smileful.config.DevelopmentConfig")
 app.config.from_object(config_path)
-db = SQLAlchemy(app)
+
 import views
 import login
 
--- smileful/config.py
+++ smileful/config.py
@@ -1,6 +1,6 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgres://xnhxsialjqhqoo:IagOEd3DJm2jMgPmG1F06HsVWH@ec2-23-21-187-45.compute-1.amazonaws.com:5432/d3dpg3es5rknub"
+    SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/smileful"
     DEBUG = True
     SECRET_KEY = os.environ.get("SMILEFUL_SECRET_KEY", "")
     
