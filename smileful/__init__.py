98e9afc7b2062a323041e1d42b506f723a55b436
George Fleming <gflemingiii@gmail.com>
2015-03-28 18:59:32 +0000

    added DB boilerplate

--- smileful/__init__.py
+++ smileful/__init__.py
@@ -1,12 +1,13 @@
 import os
 
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
 config_path = os.environ.get("CONFIG_PATH", "smileful.config.DevelopmentConfig")
 app.config.from_object(config_path)
+db = SQLAlchemy(app)
 
 import views
 import login
