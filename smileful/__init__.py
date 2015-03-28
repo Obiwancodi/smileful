e58c22a19d1d6e014d4b134f67ee4d57e9fc3b90
George Fleming <gflemingiii@gmail.com>
2015-03-28 00:57:28 +0000

    again

--- smileful/__init__.py
+++ smileful/__init__.py
@@ -4,9 +4,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 config_path = os.environ.get("CONFIG_PATH", "smileful.config.DevelopmentConfig")
 app.config.from_object(config_path)
-
+db = SQLAlchemy(app)
 import views
 import login
 
